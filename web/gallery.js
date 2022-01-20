c17bf9d8c5c3dcdd8f829eb3ca236dfdded270f0
James Keveren <james@keve.ren>
2022-01-20 01:17:17 +0000

    gallery client request aborting and comments

--- web/gallery.js
+++ web/gallery.js
@@ -1,16 +1,17 @@
 // I haven't written unit tests for this yet. This is mostly because I don't
 // have time right now given my unfamiliarity of unit testing client side JS.
 
+// steal my code
 console.info("https://github.com/jkeveren/personal-website")
 
 document.title = "Gallery | James Keveren";
 
+// put margin in bin
 Object.assign(document.body.style, {
-	background: "#000",
-	color: "#fff",
 	margin: 0
 })
 
+// create img element and style it
 let img = document.createElement("img");
 Object.assign(img.style, {
 	position: "fixed",
@@ -18,43 +19,59 @@ Object.assign(img.style, {
 	height: "100vh"
 });
 document.body.appendChild(img);
+
+// handle object URLs
 let objectURL = "";
 img.addEventListener("load", e => {
 	URL.revokeObjectURL(objectURL);
 });
 
-function getImageName(path) {
-	let parts = path.split("/");
-	return parts[parts.length - 1];
-}
-
-let imageName = getImageName("/gallery/testImage.jpg")
-let want = "testImage.jpg";
-console.assert(imageName === want, `Want: ${want}, Got: ${imageName}`)
-
 let nextImage = null;
 let prevImage = null;
+let abortController = new AbortController()
 
+// get image from server and push to img element
 async function displayImage(image, pushState) {
+	// If there's nothing to do; do nothing.
 	if (image === null) {
 		return
 	}
+
+	// SPA navigation
 	if (pushState) {
 		history.pushState({}, document.title, image);
 	}
-	let response = await fetch("/galleryImage/" + image);
+
+	// abort any previous fetch to avoid race condition
+	abortController.abort()
+	abortController = new AbortController()
+	let response = await fetch("/galleryImage/" + image, {
+		signal: abortController.signal
+	});
+
+	// get next and prev image
 	nextImage = response.headers.get("Next");
 	prevImage = response.headers.get("Previous");
+
+	// push image to img element
 	let content = await response.blob();
 	objectURL = URL.createObjectURL(content);
 	img.src = objectURL;
 }
 
+// get image name from path
+function getImageName(path) {
+	let parts = path.split("/");
+	return parts[parts.length - 1];
+}
+
+// SPA navigation
 addEventListener("popstate", e => {
 	let image = getImageName(location.pathname)
 	displayImage(image, false)
 });
 
+// Arrow keys
 addEventListener("keydown", e => {
 	if (e.key === "ArrowRight") {
 		displayImage(nextImage, true);
@@ -63,5 +80,6 @@ addEventListener("keydown", e => {
 	}
 });
 
+// display first image
 let image = getImageName(location.pathname);
 await displayImage(image, false);
